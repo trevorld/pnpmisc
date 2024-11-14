c112bd9bfb2d58dc68fef5b8b82e8e9ab95b02b0
Trevor L. Davis <trevor.l.davis@gmail.com>
2024-11-14 07:56:01 -0800

    fix(ls_temp_pdfs()): Add 'normalizePath()'

--- R/pdf_clean.R
+++ R/pdf_clean.R
@@ -51,6 +51,8 @@ pdf_clean <- function(input, output = NULL, ...) {
 #' @export
 ls_temp_pdfs <- function(exclude = character()) {
     temp_pdfs <- list.files(tempdir(), pattern = "\\.pdf$", full.names = TRUE)
+    temp_pdfs <- normalizePath(temp_pdfs, mustWork = TRUE)
+    exclude <- normalizePath(exclude, mustWork = TRUE)
     setdiff(temp_pdfs, exclude)
 }
 
