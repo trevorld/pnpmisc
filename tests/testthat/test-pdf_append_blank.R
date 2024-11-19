860a31afe73a00a0dd0afbfc8679eb2f37727210
Trevor L. Davis <trevor.l.davis@gmail.com>
2024-11-18 23:18:18 -0800

    refactor! 'pdf_pad_length()' renamed 'pdf_append_blank()'
    BREAKING CHANGES:
    
    * `pdf_pad_length()` renamed `pdf_append_blank()`

--- tests/testthat/test-pdf_append_blank.R
+++ tests/testthat/test-pdf_append_blank.R
@@ -5,21 +5,21 @@ test_that("`pdf_create_blank()`", {
     expect_equal(qpdf::pdf_length(f), 4L)
 })
 
-test_that("`pdf_pad_length()`", {
+test_that("`pdf_append_blank()`", {
     on.exit(rm_temp_pdfs(), add = TRUE)
 
     f <- pdf_create_blank(length = 1L)
     expect_equal(qpdf::pdf_length(f), 1L)
 
-    f2 <- pdf_pad_length(f, minimum = 8L, multiples_of = 4L)
+    f2 <- pdf_append_blank(f, minimum = 8L, multiples_of = 4L)
     expect_equal(qpdf::pdf_length(f2), 8L)
 
-    f3 <- pdf_pad_length(f, minimum = 1L, multiples_of = 4L)
+    f3 <- pdf_append_blank(f, minimum = 1L, multiples_of = 4L)
     expect_equal(qpdf::pdf_length(f3), 4L)
 
-    f4 <- pdf_pad_length(f, minimum = 8L, multiples_of = 3L)
+    f4 <- pdf_append_blank(f, minimum = 8L, multiples_of = 3L)
     expect_equal(qpdf::pdf_length(f4), 9L)
 
-    f5 <- pdf_pad_length(f, minimum = 1L, multiples_of = 1L)
+    f5 <- pdf_append_blank(f, minimum = 1L, multiples_of = 1L)
     expect_equal(qpdf::pdf_length(f5), 1L)
 })
