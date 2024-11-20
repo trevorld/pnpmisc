77fd15d5e123226c8f8c558d37802f5abde19ab8
Trevor L. Davis <trevor.l.davis@gmail.com>
2024-11-19 19:38:01 -0800

    refactor!: Rename {xmpdf} wrappers
    BREAKING CHANGES:
    
    * `pdf_add_bookmarks()` renamed `pdf_set_bookmarks()`
    * `pdf_add_docinfo()` renamed `pdf_set_docinfo()`
    * `pdf_add_xmpdf()` renamed `pdf_set_xmpdf()`
    
    In particular these actions may not only add metadata
    but may possibly also remove/delete metadata.

--- tests/testthat/test-xmpdf.R
+++ tests/testthat/test-xmpdf.R
@@ -7,16 +7,16 @@ test_that("{xmpdf} wrappers", {
 
     skip_if_not(xmpdf::supports_set_bookmarks())
     bm <- data.frame(title = c("Page 1", "Page 2"), page = c(1, 2))
-    f2 <- pdf_add_bookmarks(f1, bookmarks = bm)
+    f2 <- pdf_set_bookmarks(f1, bookmarks = bm)
     expect_equal(qpdf::pdf_length(f2), 2L)
 
     skip_if_not(xmpdf::supports_set_docinfo())
     di <- xmpdf::docinfo(title = "A Title", author = "The Author")
-    f3 <- pdf_add_docinfo(f1, docinfo = di)
+    f3 <- pdf_set_docinfo(f1, docinfo = di)
     expect_equal(qpdf::pdf_length(f3), 2L)
 
     skip_if_not(xmpdf::supports_set_xmp())
     xmp <- xmpdf::xmp(title = "A Title", creator = "The Author")
-    f4 <- pdf_add_xmp(f1, xmp = xmp)
+    f4 <- pdf_set_xmp(f1, xmp = xmp)
     expect_equal(qpdf::pdf_length(f4), 2L)
 })
