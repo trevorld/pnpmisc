18aaa691d6b0f4ca7b0f430af8cb80c8ecb0b964
Trevor L. Davis <trevor.l.davis@gmail.com>
2025-03-03 09:20:04 -0800

    feat(`pdf_create_blank()`): Add `grob` argument
    * The new `grob` argument of `pdf_create_blank()` is drawn on each page.
    * The default `NULL` means nothing is drawn on each page.
    * In particular can use `grob = grid::textGrob("This page intentionally left blank")`

--- R/pdf_create_blank.R
+++ R/pdf_create_blank.R
@@ -6,6 +6,9 @@
 #' @param length Number of pages to create.
 #' @param width,height Paper size in inches if `paper = "special"`.
 #' @param orientation Either "portrait" or "landscape".  Ignored if `paper = "special"`.
+#' @param grob A grid grob to draw on each page 
+#'             (e.g. `grid::textGrob("This page intentionally left blank.")`).  
+#'             Default `NULL` is to draw nothing.
 #' @return `output` pdf file name invisibly.
 #'         As a side effect creates a blank pdf file.
 #' @examples
@@ -14,11 +17,13 @@
 #' unlink(f1)
 #' unlink(f2)
 #' @export
-pdf_create_blank <- function(output = NULL, ...,
+pdf_create_blank <- function(output = NULL,
+                             ...,
                              length = 1L,
                              paper = c("special", "letter", "a4"),
                              orientation = c("portrait", "landscape"),
-                             width = 8.5, height = 11, bg = "white") {
+                             width = 8.5, height = 11, bg = "white",
+                             grob = NULL) {
     paper <- tolower(paper)
     paper <- match.arg(paper)
     orientation <- match.arg(orientation)
@@ -29,8 +34,10 @@ pdf_create_blank <- function(output = NULL, ...,
 
     pnp_pdf(output, paper = paper, orientation = orientation,
             width = width, height = height, bg = bg)
-    for (page in seq.int(length))
+    for (page in seq.int(length)) {
         grid.newpage()
+        grid.draw(grob)
+    }
     invisible(dev.off())
 
     invisible(output)
