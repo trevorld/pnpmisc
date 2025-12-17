527a4b304956abb93c391150e69085b2ec7602f4
Trevor L Davis <trevor.l.davis@gmail.com>
2025-12-16 17:01:38 -0800

    feat: `bm_create_pdf()`
    * `bm_create_pdf()` creates pdf documents from a list of (raster) images
    
    closes #33, closes #36

--- tests/testthat/test-pdf_add_rects.R
+++ tests/testthat/test-pdf_add_rects.R
@@ -3,6 +3,7 @@ test_that("`pdf_add_lines()`", {
 
 	f1 <- pdf_create_blank(length = 2L, paper = "letter")
 	f2 <- pdf_add_lines(f1, layout = "poker_3x3", dpi = 75)
+	expect_equal(qpdf::pdf_length(f2), 2L)
 })
 
 test_that("`pdf_add_rects()`", {
@@ -10,4 +11,5 @@ test_that("`pdf_add_rects()`", {
 
 	f1 <- pdf_create_blank(length = 2L, paper = "letter")
 	f2 <- pdf_add_rects(f1, layout = "poker_3x3", dpi = 75)
+	expect_equal(qpdf::pdf_length(f2), 2L)
 })
